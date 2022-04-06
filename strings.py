1d499b4bfc3c489336cabe99d5c59dfd1ace5382
Brice Tran <brice1992@msn.com>
2022-04-06 16:53:21 -0400

    fixed code for contact function

--- strings.py
+++ strings.py
@@ -1,1 +1,20 @@
-print("Hello")
+# assignment operator =
+dog = "nugget" # assigning value [nugget] to the variable ´[dog]
+# introduction 'nugget's name is nugget' # doesn't work because the string uss single quote
+# string containing single quotes needs double quotes
+introduction = "nugget's name is nugget"
+# strings with both need triple quotes
+introduction = """nugget's name is "nugget"! """
+# characters can be "escaped" using backslash
+introduction = "my dog's name is \"nugget\""
+
+# multiline strings
+# using triple quotes to use new lines
+long_introduction = """
+hello
+
+my dog's name is "nugget
+"""
+
+# use escaped newline
+long_introduction = "hello\n\nmy dog's name is \"nugget\""
